cb755e28136fdd6c2df9e516696d97eec212f9ae
Paul Adams <ghusiyy@gmail.com>
2023-04-06 12:07:00 -0700

    add Ajv

--- lib/FileData.ts
+++ lib/FileData.ts
@@ -1,3 +1,6 @@
+// TODO: move to schema + validation module
+import Ajv from "https://esm.sh/ajv@8.12.0";
+
 /**
  * Dynamically generate class from file data.
  * Example: given JSON schema
@@ -10,10 +13,26 @@
  *    "required": ["firstName", "lastName"]
  *  }
  */
+const ajv = new Ajv();
+const schema = {
+  type: "object",
+  properties: {
+    firstName: { type: "string" },
+    lastName: { type: "string" },
+  },
+  required: ["firstName", "lastName"],
+};
+const validate = ajv.compile(schema);
 const person = {
   firstName: "John",
   lastName: "Doe",
 };
+const isValid = validate(person);
+if (isValid) {
+  console.log("Valid!");
+} else {
+  console.log("Invalid!");
+}
 
 class FileData {
   constructor(data: any) {
