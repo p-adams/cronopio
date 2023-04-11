aaf30154711ff53ef9969eedbd10cc7ebe836047
Paul Adams <ghusiyy@gmail.com>
2023-04-10 22:56:29 -0700

    update docs with snippets

--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -7,27 +7,12 @@
 <p>A file based database</p>
 <section>
   <h2>Getting Started</h2>
-  <div>
+  <section>
     <h3>Define JSON Schema</h3>
-    <div>
-      Given
-      <code
-        >{@html `
-        interface Person {
-          firstName: string
-          lastName: string
-        }
-          `}</code
-      >
-      generate JSON schema
-    </div>
-
-    <ul>
-      <li>
-        <h4>JSON Schema Language</h4>
-        <pre>
-          {@html `
-const schema: SchemaType<Person> = {
+    <h4>JSON Schema Language</h4>
+    <pre>
+    {@html `
+const schema = {
   type: "object",
   properties: {
     firstName: { type: "string" },
@@ -36,26 +21,41 @@ const schema: SchemaType<Person> = {
   required: ["firstName", "lastName"],
 };`}
         </pre>
-      </li>
+
+    <h3>TypeScript</h3>
+    Generate JSON schema from interface
+    <pre>
+      {@html `
+// Define type
+
+interface Person {
+  firstName: string
+  lastName: string
+}
+      `}</pre>
+    <ul>
       <li>
         <h4>typescript-json-schema</h4>
-        <pre>
-          {@html `
-import { generateSchema } from "typescript-json-schema";
-const personSchema = generateSchema<Person>(Person);
-          `}
-        </pre>
+        <pre>{@html `import { generateSchema } from "typescript-json-schema";
+const personSchema = generateSchema(Person);`}</pre>
       </li>
       <li>
-        <h4>ts-json-schema-generator</h4>
-        <pre>
-          {@html `
-npx ts-json-schema-generator --path 'path/to/Person.ts' --type Person --noExtraProps --required   
-
-The output of this command should be a JSON schema that corresponds to the Person interface    
-          `}
-        </pre>
+        <h4>Using ts-json-schema-generator</h4>
+        <pre>{@html `npx ts-json-schema-generator --path 'path/to/Person.ts' --type Person --noExtraProps --required`}</pre>
       </li>
     </ul>
-  </div>
+    <div>
+      The output of this command should be a JSON schema that corresponds to the <code
+        >{@html `Person interface`}</code
+      >
+    </div>
+  </section>
 </section>
+
+<style>
+  pre {
+    padding: 10px;
+    width: fit-content;
+    background-color: lightgray;
+  }
+</style>
