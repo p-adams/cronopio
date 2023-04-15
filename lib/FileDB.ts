9b1ae38c7700d013f1dc5e207d09c4ea9a120322
Paul Adams <ghusiyy@gmail.com>
2023-04-15 00:50:58 -0700

    implement find operation

--- lib/FileDB.ts
+++ lib/FileDB.ts
@@ -1,5 +1,5 @@
 import { SchemaValidator, SchemaType, CollectionInterface } from "./Schema.ts";
-
+import { FileData } from "./FileData.ts";
 function writeJsonToFile<T>(data: T, path: string) {
   try {
     const encoder = new TextEncoder();
@@ -10,6 +10,12 @@ function writeJsonToFile<T>(data: T, path: string) {
   }
 }
 
+async function readJsonFile(path: string): Promise<any> {
+  const jsonString = await Deno.readTextFile(path);
+  const data = JSON.parse(jsonString);
+  return data;
+}
+
 type FileDB<T extends CollectionInterface<any>> = {
   schema: SchemaType<T>;
   data: T;
@@ -17,6 +23,7 @@ type FileDB<T extends CollectionInterface<any>> = {
   getData(): T;
   getSchema(): SchemaType<T>;
   getPath(): string;
+  find(queryObject?: Record<string, unknown>): any;
 };
 
 export function createFileDB<T extends CollectionInterface<any>>(
@@ -31,6 +38,22 @@ export function createFileDB<T extends CollectionInterface<any>>(
       schema,
       data,
       path,
+      async find(queryObj?: Record<string, unknown>) {
+        const data = await readJsonFile(this.getPath());
+        if (!queryObj) {
+          return data;
+        }
+        const filteredData = data.collection.find((item: any) => {
+          // Check if item matches queryObj
+          for (const key in queryObj) {
+            if (item[key] !== queryObj[key]) {
+              return false;
+            }
+          }
+          return true;
+        });
+        return filteredData;
+      },
       getData() {
         return this.data;
       },
@@ -46,3 +69,43 @@ export function createFileDB<T extends CollectionInterface<any>>(
     throw error;
   }
 }
+
+async function run() {
+  interface Person {
+    firstName: string;
+    lastName: string;
+  }
+  interface People extends CollectionInterface<Person> {
+    collection: {
+      firstName: string;
+      lastName: string;
+    }[];
+  }
+  const schema: SchemaType<People> = {
+    type: "object",
+    properties: {
+      collection: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            firstName: { type: "string" },
+            lastName: { type: "string" },
+          },
+          required: ["firstName", "lastName"],
+        },
+      },
+    },
+    required: ["collection"],
+  };
+  const person: People = {
+    collection: [
+      { firstName: "John", lastName: "Doe" },
+      { firstName: "Mary", lastName: "Smith" },
+    ],
+  };
+  const db = createFileDB(schema, person, "./Person.json");
+  const data = await db.find({ lastName: "Smith" });
+  console.log("data: ", data);
+}
+run();
