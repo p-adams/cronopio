27d7b7174d33f45dc38c0a16451901a7b54e5228
Paul Adams <ghusiyy@gmail.com>
2023-04-22 23:55:23 -0700

    implement $update and $delete operations

--- lib/operations.ts
+++ lib/operations.ts
@@ -101,3 +101,48 @@ export function $insert<T>(
     collection: { collection: newCollection },
   };
 }
+
+export function $update<T>(
+  data: { collection: T[] },
+  queryObj: Record<string, unknown>,
+  updateObj: Record<string, unknown>
+): { collection: T[] } {
+  const index = data.collection.findIndex((item: T) => {
+    // Check if item matches queryObj
+    return matchQuery(item, queryObj);
+  });
+  if (index === -1) {
+    throw new Error(`Object not found in collection`);
+  }
+  // Update the object at the found index
+  const updatedItem = { ...data.collection[index] };
+  for (const key in updateObj) {
+    // Access nested properties using dot notation
+    const keys = key.split(".");
+    let value: IndexableQuery = updatedItem;
+    for (let i = 0; i < keys.length - 1; i++) {
+      value = value[keys[i]];
+      if (value === undefined) {
+        throw new Error(`Property not found: ${key}`);
+      }
+    }
+    value[keys[keys.length - 1]] = updateObj[key];
+  }
+  data.collection[index] = updatedItem;
+  return { collection: data.collection };
+}
+
+export function $delete<T extends IndexableQuery>(
+  data: { collection: T[] },
+  filter: Query
+): { collection: T[] } {
+  try {
+    const filteredData = data.collection.filter(
+      (item) => !matchQuery(item, filter)
+    );
+    return { collection: filteredData };
+  } catch (err) {
+    console.error(err);
+    return { collection: data.collection };
+  }
+}
--- test/operations.test.ts
+++ test/operations.test.ts
@@ -1,5 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.182.0/testing/asserts.ts";
-import { $find, $findOne, $insert } from "../lib/operations.ts";
+import {
+  $find,
+  $findOne,
+  $insert,
+  $update,
+  $delete,
+} from "../lib/operations.ts";
 
 Deno.test("$find on shallow object properties", () => {
   assertEquals(
@@ -160,5 +166,98 @@ Deno.test("$insert multiple documents", () => {
       { id: 0, data: "foo" },
       { id: 1, data: "bar" },
     ]).collection
+  );
+});
+
+Deno.test("$update flat document property", () => {
+  const data = {
+    collection: [
+      { id: 0, product: "T-Shirt", color: "White", quantity: 10 },
+      { id: 1, product: "T-Shirt", color: "Green", quantity: 2 },
+    ],
+  };
+  assertEquals(
+    {
+      collection: [
+        { id: 0, product: "T-Shirt", color: "White", quantity: 11 },
+        { id: 1, product: "T-Shirt", color: "Green", quantity: 2 },
+      ],
+    },
+    $update(data, { id: 0 }, { quantity: 11 })
+  );
+});
+
+Deno.test("$update nested document property", () => {
+  const data = {
+    collection: [
+      {
+        id: 0,
+        name: "foo",
+        contact: {
+          address: "1234 Acme Dr.",
+          phone: { home: 222_222_2222, cell: 888_888_8888 },
+        },
+      },
+      {
+        id: 1,
+        name: "bar",
+        contact: {
+          address: "222 Acme Lane",
+          phone: { home: 222_222_2222, cell: 888_888_8888 },
+        },
+      },
+    ],
+  };
+
+  assertEquals(
+    {
+      collection: [
+        {
+          id: 0,
+          name: "foo",
+          contact: {
+            address: "1234 Acme Dr.",
+            phone: { home: 222_222_2222, cell: 888_888_8888 },
+          },
+        },
+        {
+          id: 1,
+          name: "bar",
+          contact: {
+            address: "222 Acme Lane",
+            phone: { home: 222_222_2222, cell: 333_333_3333 },
+          },
+        },
+      ],
+    },
+    $update(data, { id: 1 }, { "contact.phone.cell": 333_333_3333 })
+  );
+});
+
+Deno.test("$delete shallow document matching query", () => {
+  const data = {
+    collection: [
+      { id: 0, name: "John" },
+      { id: 1, name: "Jane" },
+    ],
+  };
+
+  assertEquals(
+    { collection: [{ id: 1, name: "Jane" }] },
+    $delete(data, { id: 0 })
+  );
+});
+
+Deno.test("$delete nested document matching query", () => {
+  const data = {
+    collection: [
+      { id: 0, name: { firstName: "John" } },
+      { id: 1, name: { firstName: "Jane" } },
+    ],
+  };
+
+  assertEquals(
+    { collection: [{ id: 1, name: { firstName: "Jane" } }] },
+    $delete(data, { "name.firstName": "John" })
   );
 });
