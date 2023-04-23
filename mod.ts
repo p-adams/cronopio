06bacedb7d77f1dfef16b43c9abc5c9979df60ec
Paul Adams <ghusiyy@gmail.com>
2023-04-23 00:51:08 -0700

    add mod file

---
+++ mod.ts
@@ -0,0 +1,7 @@
+export { createFileDB } from "./lib/FileDB.ts";
+/**
+ * Next steps:
+ *  deno bundle mod.ts bundle.js
+ *  deno publish --allow-read --allow-write --allow-net
+ *  import { createFileDB } from 'https://deno.land/x/my_package/mod.js';
+ */
